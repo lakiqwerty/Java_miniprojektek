dc122ba35b7cee30b488dac11f0797d6992b7007
lakiqwerty <lakatosbbenjaminzsolt@uni.sapientia.ro>
2024-02-20 18:19:28 +0200

    miniprojektek1_1

---
+++ miniprojekt1_1.java
@@ -0,0 +1,11 @@
+package Java_miniprojektek;
+
+public class miniprojekt1_1 {
+    public static void main(String[] args) {
+        int szam = 8;
+        for (int i = 1; i<=10; ++i)
+        {
+            System.out.printf("%d x %d = %d \n", szam, i, szam * i);
+        }
+    }
+}
