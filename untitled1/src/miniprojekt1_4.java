dafd4ef157283820f3487577fd0e819e7cfe5b04
lakiqwerty <lakatosbbenjaminzsolt@uni.sapientia.ro>
2024-02-24 14:00:43 +0200

    miniprojekt1_4

--- untitled1/src/miniprojekt1_4.java
+++ untitled1/src/miniprojekt1_4.java
@@ -1,2 +1,18 @@
-package Java_miniprojektek.untitled1.src;public class miniprojekt1_4 {
+package Java_miniprojektek.untitled1.src;
+import java.util.Scanner;
+
+public class miniprojekt1_4 {
+    public static void main(String[] args) {
+        int sorok;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("adja meg a sorok szamat: ");
+        sorok = myObj.nextInt();
+
+        for (int i = 1; i <= sorok; ++i) {
+            for (int j = 1; j <= i; ++j){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
--- untitled1/src/miniprojekt1_5.java
+++ untitled1/src/miniprojekt1_5.java
@@ -1,2 +1,13 @@
-package Java_miniprojektek.untitled1.src;public class miniprojekt1_5 {
+package Java_miniprojektek.untitled1.src;
+import java.util.Scanner;
+
+public class miniprojekt1_5 {
+    public static void main(String[] args) {
+        String szo;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Adja meg a stringet");
+        szo = myObj.nextLine();
+        boolean eredmeny = szo.matches("[a-zA-Z]+");
+        System.out.println(eredmeny + " (true: a string csak betuket tartalmaz, false: a string nem csak betuket tartalmaz)");
+    }
 }
